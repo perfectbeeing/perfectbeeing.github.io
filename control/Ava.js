dd55c51b5e0fb67f267a88d0378eb4a6555e39d8
elliot <elliot.bradly@gmail.com>
2017-11-10 17:19:32 -0500

    are you ready to ragnarock

---
+++ control/Ava.js
@@ -0,0 +1,9 @@
+var Bot = function () { var self = {};  
+var avaChoose = require("../../0-bee/2_ava/03_ava-choose.js")();
+var enableChoiceList = require("../../0-bee/2_ava/05_enable-choice-list.js")();
+var avaAction = require("../../0-bee/2_ava/06_ava-action.js")();
+var changeContextClient = require("../../0-bee/2_ava/13_change-context-client.js")();
+var actProcess = require("../../0-bee/2_ava/15_act-process.js")();
+var actSelect = require("../../0-bee/2_ava/16_act-select.js")();
+return self;} 
+exports = module.exports = Bot;
